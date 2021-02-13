908ed373a8f0abcaac6d196b86e8c15a650f6c0c
Akiicat <aaaa1379@gmail.com>
2021-02-14 02:06:31 +0800

    debug gcf

--- home/main.go
+++ home/main.go
@@ -1,13 +1,33 @@
 package home
 
 import (
+	"os"
+	"fmt"
+	"log"
   "net/http"
 )
 
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "text/html")
-  home := "/srv/vendor/home/templates/index.html"
-  http.ServeFile(w, r, home)
+	path, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Fprintln(w, path)
+
+	files, err := ioutil.ReadDir(".")
+	if err != nil {
+		http.Error(w, "Unable to read files", http.StatusInternalServerError)
+		log.Printf("ioutil.ListFiles: %v", err)
+		return
+	}
+	fmt.Fprintln(w, "Files:")
+	for _, f := range files {
+		fmt.Fprintf(w, "\t%v\n", f.Name())
+	}
+
+  // w.Header().Set("Content-Type", "text/html")
+  // home := "/srv/vendor/home/templates/index.html"
+  // http.ServeFile(w, r, home)
 }
 
